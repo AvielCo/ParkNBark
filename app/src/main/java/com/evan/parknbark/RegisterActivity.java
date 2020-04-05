a615cbb24d930b0406d45586dd8a67812eced80c
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2020-04-05 12:52:37 +0300

    Testing push

--- app/src/main/java/com/evan/parknbark/RegisterActivity.java
+++ app/src/main/java/com/evan/parknbark/RegisterActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "Register";
+    private static final String KEY_FNAME = "fname";
+    private static final String KEY_LNAME = "lname";
+    private static final String KEY_PERMISSION = "permission";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
