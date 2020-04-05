2ebabd0b31b88129ec98b64d65c5ed12a18ba298
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2020-04-05 18:45:23 +0300

    Added unit tests.

--- app/src/main/java/com/evan/parknbark/MainActivity.java
+++ app/src/main/java/com/evan/parknbark/MainActivity.java
@@ -1,18 +1,12 @@
 package com.evan.parknbark;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -24,7 +18,6 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private TextInputLayout textInputEmail, textInputPassword;
-
     private FirebaseAuth mAuth;
     private static final String TAG = "MainActivity";
 
