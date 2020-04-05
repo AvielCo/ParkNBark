fcae1b152cfdf0d76a950d3a1fc966c34a388c01
VitNecha <41152474+VitNecha@users.noreply.github.com>
2020-04-05 13:33:17 +0300

    Additional dependecies

--- app/src/main/java/com/evan/parknbark/RegisterActivity.java
+++ app/src/main/java/com/evan/parknbark/RegisterActivity.java
@@ -3,9 +3,10 @@ package com.evan.parknbark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     private static final String TAG = "Register";
     private static final String KEY_FNAME = "fname";
     private static final String KEY_LNAME = "lname";
