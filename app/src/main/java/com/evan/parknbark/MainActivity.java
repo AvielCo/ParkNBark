e8351a81192f5febe356d1a6bcf261338c0c46cc
Aviel Cohen <38301471+AvielCo@users.noreply.github.com>
2020-04-10 16:57:02 +0300

    merged master with noah.

--- app/src/main/java/com/evan/parknbark/MainActivity.java
+++ app/src/main/java/com/evan/parknbark/MainActivity.java
@@ -1,36 +1,17 @@
 package com.evan.parknbark;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
-import com.evan.parknbark.emailpassword.LoginActivity;
-import com.evan.parknbark.emailpassword.RegisterActivity;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.evan.parknbark.emailpassword.*;
+import com.evan.parknbark.google.GoogleAuthActivity;
+import com.evan.parknbark.profile.ProfileActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import es.dmoral.toasty.Toasty;
 
@@ -48,96 +29,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         mAuth = FirebaseAuth.getInstance();
         if(mAuth.getCurrentUser() != null)
             mAuth.signOut();
-
-        //build the google sign in button.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        SignInButton mButtonGoogle = findViewById(R.id.button_google_sign_in);
-        mButtonGoogle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signInWithGoogle();
-            }
-        });
-    }
-
-    public void register(View v) {
-        startActivity(new Intent(this, RegisterActivity.class));
-    }
-
-    public void login(View v) {
-        startActivity(new Intent(this, LoginActivity.class));
-    }
-
-    /**
-     * Opens a window to select a google account to log in with.
-     */
-    private void signInWithGoogle() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-    }
-
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
-            Toasty.success(this, "Signed in with google.", Toast.LENGTH_SHORT).show();
-            FirebaseGoogleAuth(acc);
-        } catch (ApiException e) {
-            Toasty.error(this, "Failed to sign in with google.", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void FirebaseGoogleAuth(final GoogleSignInAccount acc) {
-        AuthCredential authCredential = GoogleAuthProvider.getCredential(acc.getIdToken(), null);
-        mAuth.signInWithCredential(authCredential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            String fname = acc.getDisplayName(), lname = acc.getFamilyName();
-
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                            Map<String, Object> newUser = new HashMap<>();
-                            newUser.put(KEY_FNAME, fname);
-                            newUser.put(KEY_LNAME, lname);
-                            newUser.put(KEY_PERMISSION, "user");
-
-                            db.collection("users").document(acc.getEmail()).set(newUser)
-                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                        @Override
-                                        public void onSuccess(Void aVoid) {
-                                            Toast.makeText(MainActivity.this, "Successfully registered.",
-                                                    Toast.LENGTH_SHORT).show();
-                                        }
-                                    })
-                                    .addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception e) {
-                                            Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
-                                            Log.d(TAG, e.toString());
-                                        }
-                                    });
-                            updateUI(user);
-                        } else {
-                            updateUI(null);
-                        }
-                    }
-                });
-
     }
 
     @Override
