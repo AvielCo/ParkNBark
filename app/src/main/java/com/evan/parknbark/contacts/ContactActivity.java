cd66cba1995fcc43745d24c34f29e59df0f3f00d
noah <noahsolo30@gmail.com>
2020-04-27 19:43:56 +0300

    added comments and removed old code left from the 1st version of this feature

--- app/src/main/java/com/evan/parknbark/contacts/ContactActivity.java
+++ app/src/main/java/com/evan/parknbark/contacts/ContactActivity.java
@@ -1,20 +1,14 @@
 package com.evan.parknbark.contacts;
 
-
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ExpandableListView;
-
 import androidx.annotation.NonNull;
-
 import com.evan.parknbark.utilis.BaseActivity;
 import com.evan.parknbark.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -41,20 +35,17 @@ public class ContactActivity extends BaseActivity {
 
         listHashMap = new HashMap<String, List<String>>();
         listDataheader = new ArrayList<>();
-
-        getDataFromFirebase( ContactActivity.this);
-
-        expandableListView  = (ExpandableListView) findViewById(R.id.contactList);
-
-
-        expandableadapter = new ExpandableListAdapter(this, listDataheader, listHashMap);
-        expandableListView.setAdapter(expandableadapter);
-        expandableadapter.notifyDataSetChanged();
-
+        getDataFromFirebase(ContactActivity.this);
 
     }
 
-
+    /**
+     * if successful, for each iteration it pulls from the document the data needed to supply the contact class.
+     * with the contact class contactList is created with the data from the document and then added to the Hashmap.
+     * after the iterations are done the hasshmap, the list and the context are used to show on view the data as expandable list
+     *
+     * @param context gets the context ContactActivity so it can get updated in the listener
+     */
     private void getDataFromFirebase(Context context) {
         db.collection("contacts")
                 .get()
@@ -62,21 +53,18 @@ public class ContactActivity extends BaseActivity {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         List<String> listDataheader = new ArrayList<>();
-
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 Contact contact = document.toObject(Contact.class);
-                                List<String> list = new ArrayList<>();
-                                list.add(PHONE_NUM + String.valueOf(contact.getPhoneNum()));
-                                list.add(FAX + String.valueOf(contact.getFax()));
+                                List<String> contactList = new ArrayList<>();
+                                contactList.add(PHONE_NUM + String.valueOf(contact.getPhoneNum()));
+                                contactList.add(FAX + String.valueOf(contact.getFax()));
                                 listDataheader.add(document.getId());
                                 Log.d(TAG, "onSuccess: contact " + document.getId() + " " + contact.getFax() + " " + contact.getPhoneNum());
 
-                                listHashMap.put(document.getId(), list );
+                                listHashMap.put(document.getId(), contactList);
                             }
-                            expandableListView  = (ExpandableListView) findViewById(R.id.contactList);
-
-
+                            expandableListView = (ExpandableListView) findViewById(R.id.contactList);
                             expandableadapter = new ExpandableListAdapter(context, listDataheader, listHashMap);
                             expandableListView.setAdapter(expandableadapter);
                         } else {
@@ -84,35 +72,6 @@ public class ContactActivity extends BaseActivity {
                         }
                     }
                 });
-   }
-
-
-    private void initData() {
-        listDataheader = new ArrayList<>();
-        listHashMap = new HashMap<>();
-
-        listDataheader.add("municipality");
-        listDataheader.add("dog hound");
-        listDataheader.add("vets");
-
-
-        List<String> municipality = new ArrayList<>();
-        municipality.add("phone number:");
-        municipality.add("fax:");
-
-        List<String> dogHound = new ArrayList<>();
-        dogHound.add("phone number:");
-        dogHound.add("fax:");
-
-        List<String> vets = new ArrayList<>();
-        vets.add("phone number:");
-        vets.add("fax:");
-
-        listHashMap.put(listDataheader.get(0),municipality);
-        listHashMap.put(listDataheader.get(1),dogHound);
-        listHashMap.put(listDataheader.get(2),vets);
-
-
     }
 
 }
