bf1e5025f5892489f9bd93d10444034e7785fd3a
noah <noahsolo30@gmail.com>
2020-04-27 19:35:03 +0300

    updated the way ContactActivity works. reads now from the db with the help of Contact class

--- app/src/main/java/com/evan/parknbark/contacts/Contact.java
+++ app/src/main/java/com/evan/parknbark/contacts/Contact.java
@@ -1,30 +1,30 @@
 package com.evan.parknbark.contacts;
 
 public class Contact {
-    private int fax;
-    private int phoneNum;
+    private String fax;
+    private String  phoneNum;
 
-    public Contact(int fax, int phoneNum) {
+    public Contact(String fax, String phoneNum) {
         this.fax = fax;
+        this.phoneNum = phoneNum;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
         this.phoneNum = phoneNum;
     }
 
     public Contact() {
-    }
-
-    public int getFax() {
-        return fax;
-    }
-
-    public void setFax(int fax) {
-        this.fax = fax;
-    }
-
-    public int getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(int phoneNum) {
-        this.phoneNum = phoneNum;
     }
 }
