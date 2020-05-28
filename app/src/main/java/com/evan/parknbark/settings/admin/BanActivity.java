64e938d417dd4f0f0b819a1cbc278d487b7dabf4
VitNecha <vtnchk1@gmail.com>
2020-05-29 00:26:42 +0300

    Ban activity mostly completed. Few graphical changes left.

--- app/src/main/java/com/evan/parknbark/settings/admin/BanActivity.java
+++ app/src/main/java/com/evan/parknbark/settings/admin/BanActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.evan.parknbark.R;
@@ -18,8 +19,8 @@ import es.dmoral.toasty.Toasty;
 
 public class BanActivity extends BaseActivity implements View.OnClickListener {
 
-    private TextView name, email, uid;
-    private FieldPath field;
+    private FieldPath bannedField, reasonField;
+    private EditText banReason;
     private Bundle b;
 
     @Override
@@ -28,31 +29,41 @@ public class BanActivity extends BaseActivity implements View.OnClickListener {
         setContentView(R.layout.activity_ban);
         b = getIntent().getExtras();
         findViewById(R.id.ban_user_button).setOnClickListener(this);
-        name = findViewById(R.id.banned_user_name);
-        email = findViewById(R.id.banned_user_email);
-        uid = findViewById(R.id.banned_user_uid);
-        name.setText("Name: " + b.getString("name"));
-        email.setText("E-Mail: " + b.getString("email"));
-        uid.setText("UID: " + b.getString("uid"));
-        field = FieldPath.of("banned");
+        TextView name = findViewById(R.id.banned_user_name);
+        TextView email = findViewById(R.id.banned_user_email);
+        TextView uid = findViewById(R.id.banned_user_uid);
+        banReason = findViewById(R.id.ban_reason_text_input);
+        String n = getString(R.string.ban_user_name) + " " + b.getString("name"),
+            e = getString(R.string.ban_user_email) + " " + b.getString("email"),
+            u = getString(R.string.ban_user_uid) + " " + b.getString("uid");
+        name.setText(n);
+        email.setText(e);
+        uid.setText(u);
+        bannedField = FieldPath.of("banned");
+        reasonField = FieldPath.of("banReason");
     }
 
     @Override
     public void onClick(View v) {
         int i = v.getId();
         if (i == R.id.ban_user_button) {
-            db.collection("users").document(b.getString("uid")).update(field,"true").addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if (task.isSuccessful()){
-                        Toasty.info(BanActivity.this, ("THE USER WAS BANNED!") ,Toasty.LENGTH_SHORT).show();
-                        finish();
+            if (banReason.getText().toString().isEmpty()) {
+                Toasty.info(BanActivity.this, ("You have to describe a reason for a ban."), Toasty.LENGTH_SHORT).show();
+            } else {
+                db.collection("users").document(b.getString("uid"))
+                        .update(bannedField, "true", reasonField, banReason.getText().toString().trim())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toasty.info(BanActivity.this, ("THE USER WAS BANNED!"), Toasty.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toasty.info(BanActivity.this, ("Something went wrong!"), Toasty.LENGTH_SHORT).show();
+                        }
                     }
-                    else {
-                        Toasty.info(BanActivity.this, ("Something went wrong!") ,Toasty.LENGTH_SHORT).show();
-                    }
-                }
-            });
+                });
+            }
         }
     }
 }
