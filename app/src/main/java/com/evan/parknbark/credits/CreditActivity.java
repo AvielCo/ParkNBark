546f953da35e9da806e73c081ac2cae62fcef014
noah <noahsolo30@gmail.com>
2020-05-30 18:22:11 +0300

    minor updates and cleanups

--- app/src/main/java/com/evan/parknbark/credits/CreditActivity.java
+++ app/src/main/java/com/evan/parknbark/credits/CreditActivity.java
@@ -1,7 +1,6 @@
 package com.evan.parknbark.credits;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,55 +13,45 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import org.w3c.dom.Document;
 
 import mehdi.sakout.aboutpage.AboutPage;
 import mehdi.sakout.aboutpage.Element;
 
 public class CreditActivity extends BaseActivity {
     private static final String TAG = "BaseActivity";
+    private String CREDITS_COLLECTION = "credits";
+    private String OPTION_DOC = "options";
+    private String ABOUTUS = "aboutus";
+    private String CONNECT = "Connect with us";
+    private String EMAIL = "email";
+    private String FACEBOOK = "facebook";
+    private String PLAYSTORE = "playstore";
     protected FirebaseFirestore database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Element adsElement = new Element();
-        adsElement.setTitle("Advertise here");
         loadConnections();
-
     }
 
     private void loadConnections() {
-//        View aboutPage = new AboutPage(CreditActivity.this)
-//                                    .isRTL(false)
-//                                    .setImage(R.drawable.app_dog_logo_background)
-//                                    .setDescription("This should hold string about us")
-//                                    .addItem(new Element().setTitle("Version 1.0"))
-//                                    .addGroup("Connect with us")
-//                                    .addEmail("email")
-//                                    .addFacebook("facebook")
-//                                    .addPlayStore("playstore")
-//                                    .create();
-//        setContentView(aboutPage);
         this.database = FirebaseFirestore.getInstance();
-        this.database.collection("credits")
-                .document("options")
+        this.database.collection(CREDITS_COLLECTION)
+                .document(OPTION_DOC)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                         if (task.isSuccessful()) {
-                            Log.d(TAG, "onSuccess: got in");
                             DocumentSnapshot  document = task.getResult();
                             View aboutPage = new AboutPage(CreditActivity.this)
                                     .isRTL(false)
                                     .setImage(R.drawable.app_dog_logo_background)
-                                    .setDescription(document.getString("aboutus"))
+                                    .setDescription(document.getString(ABOUTUS))
                                     .addItem(new Element().setTitle("Version 1.0"))
-                                    .addGroup("Connect with us")
-                                    .addEmail(document.getString("email"))
-                                    .addFacebook(document.getString("facebook"))
-                                    .addPlayStore(document.getString("playstore"))
+                                    .addGroup(CONNECT)
+                                    .addEmail(document.getString(EMAIL))
+                                    .addFacebook(document.getString(FACEBOOK))
+                                    .addPlayStore(document.getString(PLAYSTORE))
                                     .create();
                             setContentView(aboutPage);
                         }
